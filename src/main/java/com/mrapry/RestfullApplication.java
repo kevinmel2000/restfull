32335f4c946005b392641d6e192f3abd13d42a0f
Matius Apry Prastowo <mrapry@Matiuss-MacBook-Pro.local>
2017-05-19 00:55:46 +0700

    First Commit for Rest Springboot

---
+++ src/main/java/com/mrapry/RestfullApplication.java
@@ -0,0 +1,12 @@
+package com.mrapry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullApplication.class, args);
+	}
+}
